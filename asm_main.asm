e65df3c334f5040c03101908cc8de1c7a6557788
jessica sandoval <sandovalj51506@student.vvc.edu>
2019-12-11 12:22:38 -0800

    update

--- asm_main.asm
+++ asm_main.asm
@@ -1,5 +1,6 @@
 ;
 ; file: asm_main.asm
+; author: Jessica Sandoval
 
 %include "asm_io.inc"
 ;
@@ -11,8 +12,11 @@ segment .data
         exit: equ 1
         SUCCESS: equ 0
         kernelcall: equ 80h
-
-
+	array dd 6,8,10,5
+	length dd 4
+	number dd 2
+	prompt db "please enter a number to multiply by:", 0
+	input dd 0 
 
 ; uninitialized data is put in the .bss segment
 ;
@@ -27,7 +31,27 @@ asm_main:
         enter   0,0               ; setup routine
         pusha
 ; *********** Start  Assignment Code *******************
+	mov eax, array
+	push eax
+	mov eax,[length]
+	push eax
+	mov eax, prompt
+	call print_string
+	call read_int
+	push eax
+	call multiply
 
+	pop eax
+	pop eax
+	pop eax
+	mov ecx, [length]
+	mov edx, 0
+loop2:
+	mov eax, [array+edx]
+	call print_int
+	call print_nl
+	add edx, 4
+	loop loop2
 ; *********** End Assignment Code **********************
 
         popa
@@ -35,4 +59,18 @@ asm_main:
         leave                     
         ret
 
-
+multiply: 
+	push ebp
+	mov 	ebp, esp
+	mov     ebx, [ebp+16]
+	mov 	ecx, [ebp+12]
+	mov     edx, 0
+loop1:
+	mov eax, [ebx + edx]
+	imul eax, [ebp+8]
+	mov [ebx + edx], eax
+	add edx, 4
+	loop loop1
+	
+	pop ebp
+	ret
